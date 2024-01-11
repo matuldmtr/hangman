b09b2f1d71535d4940fc90a0d05ac81e43bd9fc0
Dmitry Matul <matuldmtr@gmail.com>
2024-01-11 22:23:49 +0100

    feat: add generateKeybord and generateRandomWord functions

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import wordsList from "./words-list.json" assert { type: "json" };
+
 const showStartModal = () => {
   const body = document.querySelector("body");
 
@@ -14,7 +16,9 @@ window.addEventListener("load", showStartModal);
 const init = () => {
   const startBtn = document.getElementById("start");
   startBtn.addEventListener("click", hideModal);
-  startBtn.addEventListener("click", startGame);
+  startBtn.addEventListener("click", showMainContent);
+  startBtn.addEventListener("click", generateKeybord);
+  startBtn.addEventListener("click", generateRandomWord);
 };
 
 const hideModal = () => {
@@ -24,7 +28,7 @@ const hideModal = () => {
 
 window.addEventListener("load", init);
 
-const startGame = () => {
+const showMainContent = () => {
   const body = document.querySelector("body");
 
   body.innerHTML = `
@@ -34,26 +38,38 @@ const startGame = () => {
       <h1>Hangman game</h1>
     </div>
     <div class="game-box">
-      <ul class="word">
-        <li class="word-letter"></li>
-        <li class="word-letter guessed">A</li>
-        <li class="word-letter"></li>
-        <li class="word-letter"></li>
-        <li class="word-letter"></li>
-      </ul>
-      <h4 class="hint">
-        Hint:
-        <b
-          >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto,
-          unde.</b
-        >
+      <ul class="word"></ul>
+      <h4 class="hint">Hint: <b></b>
       </h4>
       <h4 class="guesses">Incorect guesses: <b>0 / 6</b></h4>
-      <div class="keyboard">
-        <button>a</button>
-        <button>b</button>
-      </div>
+      <div class="keyboard"></div>
     </div>
   </div>
   `;
+};
+
+const generateKeybord = () => {
+  const keybord = document.querySelector(".keyboard");
+
+  for (let i = 97; i <= 122; i += 1) {
+    keybord.innerHTML += `<button>${String.fromCharCode(i)}</button>`;
+  }
+};
+
+const generateRandomWord = () => {
+  let randomIndex = Math.floor(Math.random() * wordsList.length);
+  const { word, hint } = wordsList[randomIndex];
+
+  const wordText = document.querySelector(".word");
+  const hintText = document.querySelector(".hint b");
+
+  wordText.innerHTML = word
+    .split("")
+    .map(() => `<li class="word-letter"></li>`)
+    .join("");
+
+  hintText.innerText = hint;
+
+  // console.log(word);
+  // console.log(hint);
 };
